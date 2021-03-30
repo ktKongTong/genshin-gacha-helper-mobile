b0874415f49e5a9776f21e4d96db8661bf350409
ktKongTong <2370957629@qq.com>
2021-03-30 14:52:01 +0800

    nothing

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,14 @@
 </template>
 
 <script>
+// 统计访问量
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?87bee8b5b17784cf6b4279d264c88ad8";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
 
 export default {
   name: 'App',
