c4474db7cb0b68740860c324bc0d20dd05ec12bf
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2021-04-05 13:41:50 +0800

    test proxy

--- api/index.js
+++ api/index.js
@@ -1,18 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
   let target = 'https://hk4e-api.mihoyo.com'
-//   let target = 'https://blog.csdn.net'
     // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
     changeOrigin: true,
     pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/backend`
-      '^/api':'/event/gacha_info/api/getGachaLog',
-      // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
+      '^/apis':'/event/gacha_info/api/getGachaLog',
     }
   })(req, res)
-//   res.json({
-//     json: true
-//   })
 }
