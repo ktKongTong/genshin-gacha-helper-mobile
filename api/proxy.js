86aaaaddfa0300fee250a9f1bc4d369c826234f6
ktKongTong <2370957629@qq.com>
2021-04-05 12:47:57 +0800

    test proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
   let target = ''
-    console.log(req.url)
+    // console.log(req.url)
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   if (req.url.startsWith('/proxy')) {
