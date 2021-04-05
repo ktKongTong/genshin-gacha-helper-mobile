720a5a144b08c40ce70d507847fa28929ff64930
ktKongTong <2370957629@qq.com>
2021-04-05 12:11:49 +0800

    test proxy

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    port:3000,
-    open:true,
-    proxy: {
-      '/apis': {
-        target: 'https://hk4e-api.mihoyo.com/event/gacha_info/api/getGachaLog',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/apis/, '')
-      }
-    }
-  },
+  // server: {
+  //   port:3000,
+  //   open:true,
+  //   proxy: {
+  //     '/apis': {
+  //       target: 'https://hk4e-api.mihoyo.com/event/gacha_info/api/getGachaLog',
+  //       changeOrigin: true,
+  //       rewrite: path => path.replace(/^\/apis/, '')
+  //     }
+  //   }
+  // },
 })
