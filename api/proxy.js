95ea2b75b16e47ee35c2ff1ff67d917024311bd2
ktKongTong <44502608+ktKongTong@users.noreply.github.com>
2021-04-05 13:19:42 +0800

    test proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,26 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
-// module.exports = (req, res) => {
-//   let target = ''
-//     // console.log(req.url)
-//   // 代理目标地址
-//   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
-//   if (req.url.startsWith('/proxy')) {
-//     target = 'https://hk4e-api.mihoyo.com/event/gacha_info/api/getGachaLog'
-//   }
-
-//   // 创建代理对象并转发请求
-//   createProxyMiddleware({
-//     target,
-//     changeOrigin: true,
-//     pathRewrite: {
-//       // 通过路径重写，去除请求路径中的 `/backend`
-//       // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-//       '^/proxy/': '/'
-//     }
-//   })(req, res)
-// }
 module.exports = (req, res) => {
-  console.log(req)
   let target = 'https://hk4e-api.mihoyo.com'
     // 创建代理对象并转发请求
   createProxyMiddleware({
@@ -29,7 +8,7 @@ module.exports = (req, res) => {
     pathRewrite: {
       // 通过路径重写，去除请求路径中的 `/backend`
       // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-      '^/api/proxy/': '/event/gacha_info/api/getGachaLog'
+      '^/': '/event/gacha_info/api/getGachaLog'
     }
   })(req, res)
 //   res.json({
