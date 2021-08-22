ec9dff67e935fc07773a997e9c6659482feb62e5
ktKongTong <2370957629@qq.com>
2021-08-22 20:10:21 +0800

    test proxy

---
+++ api/index.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+module.exports = (req, res) => {
+  let target = 'https://hk4e-api.mihoyo.com'
+    // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api':'/event/gacha_info/api/getGachaLog',
+    }
+  })(req, res)
+}
--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server:{
-    proxy:{
-        '/ktapi':{
-        changeOrigin: true,
-        target:'https://hk4e-api.mihoyo.com',
-        rewrite:path => path.replace(/^\/ktapi/, ''),
-        }
-      },
+  // server:{
+  //   proxy:{
+  //       '/ktapi':{
+  //       changeOrigin: true,
+  //       target:'https://hk4e-api.mihoyo.com',
+  //       rewrite:path => path.replace(/^\/ktapi/, ''),
+  //       }
+  //     },
     
     
-  }
+  // }
 })
