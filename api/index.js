5f392b231eef04a1553582f359ddebe9ec11a92b
ktKongTong <2370957629@qq.com>
2021-08-25 12:51:29 +0800

    test proxy

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
-  let target = 'https://hk4e-api.mihoyo.com'
-    // 创建代理对象并转发请求
   createProxyMiddleware({
-    target,
+    target:'https://hk4e-api.mihoyo.com',
     changeOrigin: true,
     pathRewrite: {
       '^/api':'/event/gacha_info/api/getGachaLog',
+    },
+    Headers:{
+      Connection:"keep-alive"
     }
   })(req, res)
 }
